3e937ba7eda5d990f94059d6ac1e8c9545e6cb8d
Job Talle <jobtalle@hotmail.com>
2023-06-18 15:00:18 +0200

    Optimized

--- random.js
+++ random.js
@@ -1,22 +1,48 @@
-/**
- * A fast & simple LCG random number generator
- * @param {Number} [seed] An integer value to use as a seed
- * @constructor
- */
-const Random = function(seed = Math.floor(Math.random() * this.MODULUS)) {
-    this.n = seed;
-};
+export class Random {
+    /**
+     * Make a random seed
+     * @returns {number} A valid random seed
+     */
+    static makeSeed() {
+        return Math.floor(Math.random() * 0x100000000);
+    }
 
-Random.prototype.MULTIPLIER = 69069;
-Random.prototype.MODULUS = 2 ** 32;
-Random.prototype.INCREMENT = 1;
+    /**
+     * Construct a randomizer
+     * @param {number} seed The seed, which must be a 32-bit unsigned integer
+     */
+    constructor(seed = Random.makeSeed()) {
+        this.n = seed;
+    }
 
-/**
- * Generate a float
- * @returns {Number} A pseudorandom float in the range [0, 1>
- */
-Random.prototype.getFloat = function() {
-    this.n = (this.MULTIPLIER * this.n + this.INCREMENT) % this.MODULUS;
+    /**
+     * Shuffle an array using this randomizer
+     * @param {Array} array The array to shuffle
+     * @returns {Array} The shuffled array
+     */
+    shuffle(array) {
+        for (let index = array.length; index-- > 0;) {
+            const randomIndex = Math.floor(this.float * (index + 1));
 
-    return this.n / this.MODULUS;
-};
+            [array[index], array[randomIndex]] = [array[randomIndex], array[index]];
+        }
+
+        return array;
+    }
+
+    /**
+     * Get a randomized integer in the range [0, 0xFFFFFFFF]
+     * @returns {number} A random integer
+     */
+    get integer() {
+        return this.n = 69069 * this.n + 1 >>> 0;
+    }
+
+    /**
+     * Get a randomized number in the range [0, 1]
+     * @returns {number} A random number
+     */
+    get float() {
+        return this.integer / 0x100000000;
+    }
+}
